eb2f4d2371cd2e7c9b01ccaabf63738f9deb34d1
Hebert Rocha <hebertrocha76@gmail.com>
2025-07-21 17:06:47 -0300

    feat: Audio recording and Gemini integration

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
-    DATABASE_URL: z.string().url().startsWith('postgressql://')
+    DATABASE_URL: z.string().url().startsWith('postgressql://'),
+    GEMINI_API_KEY: z.string(),
 })
 
 export const env = envSchema.parse(process.env);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors';
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify';
 import {
     serializerCompiler,
@@ -10,12 +11,15 @@ import { createQuestionRoute } from './http/routes/create-question.ts';
 import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomQuestions } from './http/routes/get-room-question.ts';
 import { getRoomRouter } from './http/routes/get-rooms.ts';
+import { uploadAudioRoute } from './http/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 });
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -28,5 +32,6 @@ app.register(getRoomRouter);
 app.register(getRoomQuestions);
 app.register(createRoomRoute);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
